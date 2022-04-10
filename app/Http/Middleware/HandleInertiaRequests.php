9a10341aa6831c6fecbdcae8579c967bb59f1b96
Allan Alvarez Ortiz <flick36@gmail.com>
2022-04-10 00:10:01 -0500

    feat: add Inertia Asset versioning

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -24,7 +24,7 @@ class HandleInertiaRequests extends Middleware
      */
     public function version(Request $request): ?string
     {
-        return parent::version($request);
+        return vite()->getHash();
     }
 
     /**
