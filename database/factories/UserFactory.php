b03b77131cc0eca6ff4cbf9aca99c1b0e9adee71
Allan Alvarez Ortiz <flick36@gmail.com>
2022-04-16 17:10:48 -0500

    chore(feat): Use argon2id instead of bcrypt

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,7 +31,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$argon2id$v=19$m=1024,t=2,p=2$ekxkYk1wNmxSellHRDZ2Qw$1F2Z8YhD3QrbSCGls15fQw', // password
             'remember_token' => Str::random(10),
         ];
     }
