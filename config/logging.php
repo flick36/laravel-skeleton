10d84e701b677a702e0552a73ca8e7e6fe39a3f8
Allan Alvarez Ortiz <flick36@gmail.com>
2022-07-19 12:01:48 -0500

    chore: upstream update

--- config/logging.php
+++ config/logging.php
@@ -81,7 +81,7 @@ return [
         'papertrail' => [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => SyslogUdpHandler::class,
+            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
